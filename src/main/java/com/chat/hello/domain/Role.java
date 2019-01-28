b2a807ec5c994bd972f18ae73cff618f43eaa48f
Денис <45272541+cheechman123@users.noreply.github.com>
2019-01-28 23:50:42 +0200

    Add files via upload

--- src/main/java/com/chat/hello/domain/Role.java
+++ src/main/java/com/chat/hello/domain/Role.java
@@ -1,10 +1,11 @@
 package com.chat.hello.domain;
 
 import org.springframework.security.core.GrantedAuthority;
-
+//перечисление для содержания ролей пользователей
 public enum Role implements GrantedAuthority {
     USER, ADMIN;
 
+    //возвращает значение роли (обозначено как USER, ADMIN...)
     @Override
     public String getAuthority() {
         return name();
